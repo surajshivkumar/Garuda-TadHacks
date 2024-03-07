b47023ce31b84d421cee21b2cca3741de72023ac
surajshivakumar <surajshivakumar53@gmail.com>
2024-03-07 18:58:06 -0500

    Remove bin, include, and lib directories from tracking

--- src/app.py
+++ src/app.py
@@ -1,12 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, request,jsonify,render_template
+from flask_cors import CORS
+import os
+from vidCon.videoCon import GetVcon
+
+from werkzeug.utils import secure_filename
+
+
 
 app = Flask(__name__)
+CORS(app)
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
+@app.route('/home')
 def home():
     return render_template('index.html')
 
+@app.route('/upload', methods=['POST','GET'])
+def upload_file():
+    global file
+    if 'file' not in request.files:
+        return 'No file part', 400
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file', 400
+    if file:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return jsonify('File successfully uploaded', 200)
+    
 
+@app.route('/vcon', methods=['GET'])
+def showVcon():
+    file = os.listdir('./uploads')[0]
+    v = GetVcon(f'./{UPLOAD_FOLDER}/{file}')
+    v.read_audio()
+    v.transcribe_audio()
+    # os.remove(f'./{UPLOAD_FOLDER}/{file}')
+    return jsonify({'transcribed':v.vcon_['transcribed']['text']})
+
+
+@app.route('/analyze', methods=['GET'])
+def Analyze():
+    return render_template('analyze.html')
+
+
+    
 if __name__ == '__main__':
     app.run(debug=True)
-
---
+++ src/static/js/script.js
@@ -0,0 +1,12 @@
+document
+  .querySelector(".sidebar-icon")
+  .addEventListener("mouseover", function () {
+    this.querySelector("i").classList.add("fas", "fa-info-circle"); // Change to info icon on hover
+  });
+
+document
+  .querySelector(".sidebar-icon")
+  .addEventListener("mouseout", function () {
+    this.querySelector("i").classList.remove("fas", "fa-info-circle"); // Revert to original icon
+    this.querySelector("i").classList.add("fas", "fa-home"); // Ensure the original icon class is added back
+  });
